3de8cea6b0a25b396c0eeba199533eeecfc10b8b
gogocat <adamchow2326@yahoo.com.au>
2015-02-27 21:28:44 +1100

    update unit test

--- test/spec/jquery-declear-spec.js
+++ test/spec/jquery-declear-spec.js
@@ -1,7 +1,11 @@
 describe("jQuery Declear - $deClear", function() {
+	
+	$deClear($.fn.accordion).use.option({header: "> div > h3"});
+	
+	it("should be a global function", function() {
+		expect(typeof $deClear).toBe("function");
+	});
 
-  it("should be a global function", function() {
-    expect(typeof $deClear).toBe("function");
-  });
 
 });
+
