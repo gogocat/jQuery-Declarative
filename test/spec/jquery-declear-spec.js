3021deff50c21a2616ce79aaa8ec28aede9c7b6a
gogocat <adamchow2326@yahoo.com.au>
2015-03-01 00:34:44 +1100

    updated test

--- test/spec/jquery-declear-spec.js
+++ test/spec/jquery-declear-spec.js
@@ -1,8 +1,11 @@
 describe("jQuery Declear - $deClear", function() {
-	var plugin,
+	var accdHeaderSelector = "> div > h3",
+		accdSelector = "[data-ui-accordion]",
+		$accd = $(accdSelector),
+		plugin,
 		pluginHeaderOption;
 		
-	$deClear($.fn.accordion).use.option({header: "> div > h3"});
+	$deClear($.fn.accordion).use.option({header: accdHeaderSelector});
 	
 	beforeEach(function() {
 		plugin = $("#accordion").data().uiAccordion,
@@ -18,9 +21,22 @@ describe("jQuery Declear - $deClear", function() {
 	});
 
 	it("Element jquery plugin options should be '> div > h3' ", function() {
-		expect(pluginHeaderOption).toEqual("> div > h3");
+		expect(pluginHeaderOption).toEqual(accdHeaderSelector);
 	});
 	
+	it("Element jquery plugin should be an instance of $deClear", function() {
+		expect(instanceof plugin).toEqual($deClear);
+	});
+	
+	it("accordion plugin original option setter should still work", function() {
+		$accd.accordion( "option", "animate", 200 );
+		expect(plugin.options.animate).toEqual(200);
+	});
+	
+	it("accordion plugin original method should still work", function() {
+		$accd.accordion( "disable" );
+		
+	});
 	
 });
 
