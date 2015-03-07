3cd1f4533a4df7795d50d55c8f47d03a8b816387
gogocat <adamchow2326@yahoo.com.au>
2015-03-07 14:08:36 +1100

    update

--- js/jquery-declear.js
+++ js/jquery-declear.js
@@ -1,6 +1,6 @@
 
 
-(function($) {
+(function($, env) {
 	"use strict";
 	// keep jQuery native $.fn.attr
 	var oldAttr = $.fn.attr,
@@ -11,10 +11,50 @@
 			var self = this;
 			if (typeof selector === "string" && typeof plugin === "string") {
 				self.use = new Use(self);
-				return self init(selector, plugin);
+				return self.init(selector, plugin);
 			}
 		};
   
+	// jQueryDeclear
+	jQueryDeclear.prototype = {
+		pluginOption: null,
+		init: function(selector, plugin) {
+			var self = this;
+			self.pluginSelector = $.trim(selector);
+			self.element = $(self.pluginSelector);
+			if (self.element.length) {
+				self.pluginOption = self.getAttrOptions();
+				return self.assignPlugin();
+			}
+		},
+		getAttrOptions: function() {
+			var attrOptions = self.element.attr(self.pluginSelector),
+				ret = null;
+			if(attrOptions) {
+				ret = evalAttr(attrOptions);
+			}
+			return ret; 
+		},
+		assignPlugin: function() {
+			self.element[plugin](self.pluginOption);
+		}
+	};
+	
+	Use.prototype = {
+		option: function(option) {
+			var self = this;
+			if ($.isPlainObject(option)) {
+				self.ctx.pluginOption = self.ctx.pluginOption || {};
+				$.extend(self.ctx.pluginOption, option);
+			}
+			return self.ctx;
+		},
+		observer: function() {
+			var self = this;
+			return self.ctx;
+		}
+	};
+	
 	// evaluate attr sting options to javascript object
 	// @opt example "{sample:'name', sampleBoolean: true}"
 	// @return {sample:'name', sampleBoolean: true}
@@ -39,43 +79,9 @@
 		}
 	});
 	
-	// jQueryDeclear
-	jQueryDeclear.prototype = {
-		pluginOption: null,
-		init: function(selector, plugin) {
-			var self = this;
-			
-			self.pluginSelector = $.trim(pluginSelector);
-			self.element = $(self.pluginSelector);
-			self.pluginOption = self.getAttrOptions();
-			if (self.element.length) {
-				return self.assignPlugin();
-			}
-		},
-		getAttrOptions: function() {
-			var attrOptions = self.element.attr(self.pluginSelector),
-				ret = null;
-			if(attrOptions) {
-				ret = evalAttr(attrOptions);
-			}
-			return ret; 
-		},
-		assignPlugin: function() {
-			self.element[plugin](self.pluginOption);
-		}
+	// set $deClear to global
+	env.$deClear = function(selector, plugin) {
+		return new jQueryDeclear(selector, plugin);
 	};
 	
-	Use.prototype = {
-		option: function(option) {
-			if ($.isPlainObject(option)) {
-				self.ctx.pluginOption = self.ctx.pluginOption || {};
-				$.extend(self.ctx.pluginOption, option);
-			}
-			return this;
-		},
-		observer: function() {
-			return this;
-		}
-	};
-	
-})(jQuery);
+})(jQuery, this);
