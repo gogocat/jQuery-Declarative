97c67031a1427070554a2adeb7f7730ea0640619
gogocat <adamchow2326@yahoo.com.au>
2015-03-08 13:38:09 +1100

    updated use JSOL parser

--- js/jquery-declear.js
+++ js/jquery-declear.js
@@ -1,4 +1,12 @@
-
+/* JSOL
+ * Copyright 2010, Google Inc.
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Google Inc. nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 (function($, env) {
 	"use strict";
@@ -16,7 +24,9 @@
 				self.attrSelector = self.pluginSelector.replace("[","").replace("]","");
 				return self;
 			}
-		};
+		},
+		trim =  /^(\s|\u00A0)+|(\s|\u00A0)+$/g,
+		JSOL = {};
   
 	// jQueryDeclear
 	jQueryDeclear.prototype = {
@@ -61,20 +71,37 @@
 		}
 	};
 	
+	/*
+	* JSOL [https://github.com/daepark/JSOL]
+	*/
+	JSOL.parse = function(text) {
+		// make sure text is a "string"
+		if (typeof text !== "string" || !text) {
+			return null;
+		}
+		// Make sure leading/trailing whitespace is removed
+		text = text.replace(trim, "");
+		// Make sure the incoming text is actual JSOL (or Javascript Object Literal)
+		// Logic borrowed from http://json.org/json2.js
+		if ( /^[\],:{}\s]*$/.test(text.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g, "@")
+			.replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, "]")
+			.replace(/(?:^|:|,)(?:\s*\[)+/g, ":")
+			/** everything up to this point is json2.js **/
+			/** this is the 5th stage where it accepts unquoted keys **/
+			.replace(/\w*\s*\:/g, ":")) ) {
+			return (new Function("return " + text))();
+		}
+		else {
+			//throw("Invalid JSOL: " + text);
+		}
+	};
+	
+	
 	// evaluate attr sting options to javascript object
 	// @opt example "{sample:'name', sampleBoolean: true}"
 	// @return {sample:'name', sampleBoolean: true}
 	function evalAttr(opt) {
-		var fnbody = 'return ', fn, ret = {};
-		if (typeof opt !== "string") {
-			return ret;
-		}
-		try {
-			fn = new Function(fnbody + opt + ";");
-			ret = fn();
-		} catch(err) {
-			return ret;
-		}
+		var ret = JSOL.parse(opt) || {};
 		return ret;
 	}
 	
